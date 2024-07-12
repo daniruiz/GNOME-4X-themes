800a12d4b8d65caa2dde0f94d987572a2e3186c7
daniruiz <daniel@drasite.com>
2024-07-12 17:40:21 +0200

    Copy uninstall script during installation

--- src/install.sh
+++ src/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 mkdir -p ~/.config/gtk-4.0/
-cp -r -f libadwaita/* ~/.config/gtk-4.0/
+cp -r -f libadwaita/* uninstall.sh ~/.config/gtk-4.0/
 
 mkdir -p ~/.themes/GNOME-4X
-cp -a gtk-* ~/.themes/GNOME-4X/
+cp -a gtk-* uninstall.sh ~/.themes/GNOME-4X/
 
 original_theme="$(gsettings get org.gnome.desktop.interface gtk-theme | tr -d \')"
 sed -i "s/Adwaita/${original_theme}/" uninstall.sh
