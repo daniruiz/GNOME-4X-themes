287c2e16758af831dd6db6bb523ef6d1fe70e5ff
daniruiz <daniel@drasite.com>
2024-07-12 17:37:40 +0200

    Revert to previous theme during uninstall

--- src/install.sh
+++ src/install.sh
@@ -6,4 +6,6 @@ cp -r -f libadwaita/* ~/.config/gtk-4.0/
 mkdir -p ~/.themes/GNOME-4X
 cp -a gtk-* ~/.themes/GNOME-4X/
 
+original_theme="$(gsettings get org.gnome.desktop.interface gtk-theme | tr -d \')"
+sed -i "s/Adwaita/${original_theme}/" uninstall.sh
 gsettings set org.gnome.desktop.interface gtk-theme "GNOME-4X"
