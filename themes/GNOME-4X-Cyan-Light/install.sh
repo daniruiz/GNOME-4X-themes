f33bdf1c4b7a1c1fabd1010f1535131540b4f5a9
daniruiz <daniel@drasite.com>
2024-07-12 17:44:11 +0200

    Generate all theme variants

--- themes/GNOME-4X-Cyan-Light/install.sh
+++ themes/GNOME-4X-Cyan-Light/install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 mkdir -p ~/.config/gtk-4.0/
-cp -r -f libadwaita/* ~/.config/gtk-4.0/
+cp -r -f libadwaita/* uninstall.sh ~/.config/gtk-4.0/
 
 mkdir -p ~/.themes/GNOME-4X
-cp -a gtk-* ~/.themes/GNOME-4X/
+cp -a gtk-* uninstall.sh ~/.themes/GNOME-4X/
 
+original_theme="$(gsettings get org.gnome.desktop.interface gtk-theme | tr -d \')"
+sed -i "s/Adwaita/${original_theme}/" uninstall.sh
 gsettings set org.gnome.desktop.interface gtk-theme "GNOME-4X"
