f6e1e97499a0312c52c9781e32f1d49062bbae9f
Daniel Ruiz de Alegría <daniel@drasite.com>
2024-07-30 16:14:22 +0200

    Update all themes

--- themes/GNOME-4X-Blue-Dark/uninstall.sh
+++ themes/GNOME-4X-Blue-Dark/uninstall.sh
@@ -2,6 +2,7 @@
 
 rm -rf ~/.config/gtk-4.0/gtk.css \
        ~/.config/gtk-4.0/assets \
+       ~/.config/gtk-4.0/uninstall.sh \
        ~/.themes/GNOME-4X/
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
--- themes/GNOME-4X-Magenta-Dark/install.sh
+++ themes/GNOME-4X-Magenta-Dark/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+original_theme="$(gsettings get org.gnome.desktop.interface gtk-theme | tr -d \')"
+sed -i "s/Adwaita/${original_theme}/" uninstall.sh
+
 mkdir -p ~/.config/gtk-4.0/
 cp -r -f libadwaita/* uninstall.sh ~/.config/gtk-4.0/
 
 mkdir -p ~/.themes/GNOME-4X
 cp -a gtk-* uninstall.sh ~/.themes/GNOME-4X/
 
-original_theme="$(gsettings get org.gnome.desktop.interface gtk-theme | tr -d \')"
-sed -i "s/Adwaita/${original_theme}/" uninstall.sh
 gsettings set org.gnome.desktop.interface gtk-theme "GNOME-4X"
