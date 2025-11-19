0cb735f48468789ae0cfd932a967b2b6ec4cae88
daniruiz <daniel@drasite.com>
2025-11-19 14:32:36 +0100

    Regenerate all themes

--- themes/GNOME-4X-Blue-Dark/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -rf ~/.config/gtk-4.0/gtk.css \
-       ~/.config/gtk-4.0/assets \
-       ~/.config/gtk-4.0/uninstall.sh \
-       ~/.themes/GNOME-4X/
-
-gsettings set org.gnome.desktop.interface gtk-theme "GNOME-4X"
