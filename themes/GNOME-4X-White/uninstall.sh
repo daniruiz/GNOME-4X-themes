c90d293b85a777cfb31a1472fd98f5b0152290e0
daniruiz <daniel@drasite.com>
2025-11-21 10:35:16 +0100

    Respect theme accent color

--- themes/GNOME-4X-White/uninstall.sh
+++ themes/GNOME-4X-White/uninstall.sh
@@ -5,4 +5,4 @@ rm -rf ~/.config/gtk-4.0/gtk.css \
        ~/.config/gtk-4.0/uninstall.sh \
        ~/.themes/GNOME-4X/
 
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
+gsettings set org.gnome.desktop.interface gtk-theme "GNOME-4X"
